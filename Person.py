ebb7fde1b6a5337a2f286370a74a90dff06fa8bb
juanitoetc <juanignacio.robledo@gmail.com>
2018-04-15 22:03:31 +0200

    Add class person. Add example

---
+++ Person.py
@@ -0,0 +1,30 @@
+'''
+Created on Apr 14, 2018
+@author: juanitoetc
+@summary: simple Person class
+'''
+
+print("\nloading module...")			#This is absolutly not necessary and should be removed.
+									#helps to understand that the module will not only laod, but also initilize the module.
+
+class Person(object):
+
+    def __init__(self, firstname, lastname, age):
+        #For each class we need the constructor
+        #this is where each instance is created
+        #return an instance of the class
+        self.strFirstName = firstname
+        self.strLastName = lastname
+        self.intAge = age
+    
+    def __str__(self):
+        #method used to print the instance, usign print(<instance>)
+        return self.strFirstName + " " + self.strLastName + ", " +str(self.intAge)
+    
+    def __eq__(self, other):
+        #method used to compare to instances
+        #return boolan True or False
+        return self.strLastName==other.strLastName and self.strFirstName==other.strFirstName and  self.intAge==other.intAge
+
+print("module loaded")			#This is absolutly not necessary and should be removed.
+									#helps to understand that the module will not only laod, but also initilize the module.
---
+++ example.py
@@ -0,0 +1,25 @@
+'''
+Created on Apr 13, 2018
+@author: juanioetc
+@sumary: just a short example of the usage of imp
+@attention: this method will be probably deprecated, see issue https://bugs.python.org/issue14551
+'''
+
+#there is no need to import a complete py using from ... import ...
+
+import imp
+
+print("Usage of the imp library, load_source in particular")
+try:
+	#Try to import the module located in the path given
+	impPerson = imp.load_source("classPerson","./Person.py")
+except IOError as errorImport:
+	#Exception handler: file not found, inform this using console and skip this module
+	print("File not imported, error: ", errorImport)
+else:
+	#import sucessfull cp = class Person
+	cpPerson1 = impPerson.Person("Juan", "Robledo", 27)	#constructor of the class in the module imported.
+	print("\n")
+	print(cpPerson1)	#using __str__ in the class declaration
+finally:
+	print("End of the example")
